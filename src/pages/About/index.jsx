ead146bcdb104f249bf80d008396742ba20b86dc
liyingjing <liyingjing@163.com>
2022-05-27 14:33:38 +0800

    22

--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -1,45 +1,45 @@
-import React, { Component, useState, useEffect ,useCallback, useMemo,useRef} from 'react';
+import React, { Component, useState, useEffect, useCallback, useMemo, useRef } from 'react';
 import { AlertOutlined, ApiOutlined, AppstoreAddOutlined, RedditOutlined } from '@ant-design/icons'
-import { useSelector,useDispatch } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import './index.css'
 import { allAbout } from "../../api/login";
 import Echarts1 from './echarts1'
 import Echarts2 from './echarts2'
-import Echarts3 from './echarts3' 
-export const TestContext =React.createContext()
+import Echarts3 from './echarts3'
+export const TestContext = React.createContext()
 function About() {
-    const [v,setv]=useState(0)
-    const [m,setm]=useState(0)
+    const [v, setv] = useState(0)
+    const [m, setm] = useState(0)
     const [unm, serNum] = useState({ num: {} })
-const dispatch=useDispatch()
-    useSelector(state=>{
-        console.log(state,'state')
+    const dispatch = useDispatch()
+    useSelector(state => {
+        console.log(state, 'state1123')
     })
-  const getDoub=useMemo(()=>{
-        console.log('getDoub调用了')
-        return m*2
-    },[m])
+    const getDoub = useMemo(() => {
+        console.log('getDoub调用2323了')
+        return m * 2
+    }, [m])
     useEffect(() => {
         allAbout().then(res => {
             serNum(res.data)
-            console.log(res,4)
+            console.log(res, 4)
         })
     }, [])
-   
-    const refs1=useRef()
-    console.log(refs1,'ref1')
-    const hanc1=useCallback(()=>{
-        setv(v+1)
+
+    const refs1 = useRef()
+    console.log(refs1, 'ref1')
+    const hanc1 = useCallback(() => {
+        setv(v + 1)
         dispatch({
-             type:'SWITCH_MEUN',
-             menuName: '修改后的MEUN'
-          })
-    },[v])
-    const hanc2=useCallback(()=>{
-        setv(m+1)
-    },[m])
-   
-      // function getDoub(){
+            type: 'SWITCH_MEUN',
+            menuName: '修改后的MEUN'
+        })
+    }, [v])
+    const hanc2 = useCallback(() => {
+        setv(m + 1)
+    }, [m])
+
+    // function getDoub(){
     //     console.log('getDoub调用了')
     //     return v*2
     // }
@@ -47,8 +47,8 @@ const dispatch=useDispatch()
         <div>
             <header className='header'>
                 <div className="cont">
-                <button onClick={hanc1}>加++{v}</button>
-                {/* <button onClick={hanc2}>加++{m}</button> */}
+                    <button onClick={hanc1}>加++{v}</button>
+                    {/* <button onClick={hanc2}>加++{m}</button> */}
                     <div ref={refs1}>医疗机构总数<div className="num_span">{unm.num1} 家{getDoub}</div></div>
                     <div>
                         <AlertOutlined className='AlertOutlined' />
@@ -75,9 +75,9 @@ const dispatch=useDispatch()
             </header>
             <div className='actionc_diyljg'>
                 <div className='center_ech'>
-                        <TestContext.Provider value={v}>
-                            <Echarts1 />
-                        </TestContext.Provider>
+                    <TestContext.Provider value={v}>
+                        <Echarts1 />
+                    </TestContext.Provider>
                 </div>
                 <div className='center_ech  ech2'>
                     <Echarts2 />
